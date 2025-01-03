6821279754c5a89a91572a1675151aea96d84469
Ali Khadempour <ali.khadempour@outlook.com>
2025-01-03 20:04:11 +0330

    feat: add API routes for service_done, update_self, person_injury, and person_death

--- src/api/routes.py
+++ src/api/routes.py
@@ -17,23 +17,51 @@ def register():
         eavs[eav.id] = eav
 
     response = world_model.register(entity.entity_type, entity.max_capacity, eavs)
-
     return jsonify(response, status=200, mimetype='application/json')
 
 
 @api.route('/api/snapshot', methods=['GET'])
 def snapshot():
     data = request.json
-
     response = world_model.snapshot(data['entity_id'])
-
     return jsonify(response.to_dict(), status=200, mimetype='application/json')
 
 
 @api.route('/api/accept-person', methods=['POST'])
 def accept_person():
     data = request.json
-
     response = world_model.accept_person(data['entity_id'], data['persons_id'])
+    return jsonify({"occur": response}, status=200, mimetype='application/json')
 
+
+@api.route('/api/service-done', methods=['POST'])
+def service_done():
+    data = request.json
+    response = world_model.service_done(data['entity_id'], data['persons_id'])
+    return jsonify({"occur": response}, status=200, mimetype='application/json')
+
+
+@api.route('/api/update-self', methods=['PUT'])
+def update_self():
+    data = request.json
+    response = world_model.update_self(data['entity_id'], data['max_capacity'], data['eavs'])
+    return jsonify({"occur": response}, status=200, mimetype='application/json')
+
+
+@api.route('/api/log', methods=['POST'])
+def log():
+    pass
+
+
+@api.route('/api/person-injury', methods=['POST'])
+def person_injury():
+    data = request.json
+    response = world_model.person_injury(data['entity_id'], data['persons_id'])
+    return jsonify({"occur": response}, status=200, mimetype='application/json')
+
+
+@api.route('/api/person-death', methods=['POST'])
+def person_death():
+    data = request.json
+    response = world_model.person_death(data['entity_id'], data['persons_id'])
     return jsonify({"occur": response}, status=200, mimetype='application/json')
